21e4697eb0a3e081b559a43a661c0cd81cb41dc4
mikeOde <mike.r.ayoub@gmail.com>
2021-11-02 17:17:03 +0200

    trial

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="efarm">
+  <BrowserRouter basename="/efarm">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
