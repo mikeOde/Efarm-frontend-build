b4a96dbc78c67604b2b1b1581ff5304ac6b912db
mikeOde <mike.r.ayoub@gmail.com>
2021-11-02 16:28:54 +0200

    trial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ProtectedCustomerRoute from "./components/protected-routes/ProtectedFarme
 function App() {
   return (
     <Switch>
-      <Route path="/efarm" exact component={RegistrationForm} />
+      <Route path="/" exact component={RegistrationForm} />
 
       <ProtectedFarmerRoute path="/efarm/profile" component={Profile} />
       <ProtectedFarmerRoute path="/efarm/dashboard" component={Dashboard} />
