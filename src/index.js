e764746edebc34db6bca3ed434a7ba6e1918389b
mikeOde <mike.r.ayoub@gmail.com>
2021-11-02 16:13:47 +0200

    trial

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="https://mikeode.github.io/efarm">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
